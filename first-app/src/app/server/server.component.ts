3faa51dd0f48e53b1a9689f1222cce85f5823400
Shaun Jacobsen <sjacobsen@grubhub.com>
2018-09-07 13:31:59 -0500

    Working on components

---
+++ first-app/src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-server', // the html selector where the component is to be rendered
+  templateUrl: './server.component.html' // the html file containing the component code
+})
+
+export class ServerComponent {
+
+}
